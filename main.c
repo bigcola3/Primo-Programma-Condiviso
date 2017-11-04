8afe8c2b957bb1b7b96d3b128a2d2ac45cde5b80
Salvatore Torsello <salvatore.torsello08@gmail.com>
2017-11-04 12:59:08 +0100

    Primo Ciclo For
    Ho inserito un ciclo for per contare i numeri da 0 a 100

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 100
 
 int main()
 {
-	printf("Hello World!");
+	int i = 0;
+	printf("Numeri da %d a %d:\n", i, MAX);
+
+	for (i=0; i <= MAX; i++)
+	{	
+		printf("%d\n", i);
+	}
 
 	return 0;
 }
